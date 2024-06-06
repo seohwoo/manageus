080b5ac24ab12a5d9a7aa47e37a4b1ae752f6931
seohwoo <tjguddn9899@naver.com>
2024-06-06 18:43:37 +0900

    feat: Setting Spring Project

---
+++ src/main/java/com/project/manageus/ManageusApplication.java
@@ -0,0 +1,13 @@
+package com.project.manageus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageusApplication.class, args);
+	}
+
+}
