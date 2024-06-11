2e5c69e8b67c5cf3ad640efd1e8031413d7e2093
seohwoo <tjguddn9899@naver.com>
2024-06-11 15:05:20 +0900

    build: Gradle Setting and properties Setting

---
+++ src/main/java/com/project/manageus/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.project.manageus.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+
+        http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/", "/login", "/register", "/forgot",
+                        "/css/**", "/js/**", "/img/**", "/scss/**", "/vendor/**").permitAll()
+                .requestMatchers("/super/**").hasRole("SUPER")
+                .requestMatchers("/admin/**").hasAnyRole("SUPER", "ADMIN")
+                .anyRequest().authenticated()
+        );
+
+        http.formLogin((auth) -> auth.loginPage("/login")
+                .loginProcessingUrl("/user/loginProc")
+                .permitAll()
+        );
+
+        http.csrf((auth) -> auth.disable());
+
+
+        return http.build();
+    }
+
+
+
+}
