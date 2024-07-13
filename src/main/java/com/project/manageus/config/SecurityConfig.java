a6a90f5abb62582a7619b1082be8562f6badbd5f
seohwoo <tjguddn9899@naver.com>
2024-07-13 17:24:01 +0900

    fix: board URL setting

--- src/main/java/com/project/manageus/config/SecurityConfig.java
+++ src/main/java/com/project/manageus/config/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
                 .requestMatchers("/admin/**").hasAnyRole("SUPER", "ADMIN")
                 .requestMatchers("/h2-console/**").permitAll()
                 .requestMatchers("/search-address").permitAll()
+                .requestMatchers("/board").permitAll()
                 // Swagger 관련 리소스 접근 허용
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**").permitAll()
                 .anyRequest().authenticated()
